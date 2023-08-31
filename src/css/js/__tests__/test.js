84b64c962b5de26b171b210632ed9bad248ba774
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-08-31 17:57:48 +0300

    Delete src/css/js/__tests__/test.js

--- src/css/js/__tests__/test.js
+++
@@ -1,9 +0,0 @@
-import getBuffer from '../basic';
-import ArrayBufferConverter from '../arraybufferconverter';
-
-test('should return string', () => {
-  const newBuff = new ArrayBufferConverter();
-  newBuff.load(getBuffer());
-
-  expect(newBuff.toString()).toMatch('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
-});
